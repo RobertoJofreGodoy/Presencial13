d0626c514de1785db71828cf6bdf1f3c329bb1e4
RobertoJofreGodoy <roberto.jofre.godoy@gmail.com>
2018-06-12 19:36:08 -0400

    add Ejercicio2 final version

--- Ejercicio2.rb
+++ Ejercicio2.rb
@@ -1,3 +1,12 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
-
 productos.each { |producto, valor| puts " El producto #{producto} vale: #{valor}" }
+
+productos[:cereal] =  2200
+productos['bebida'] = 2000
+puts productos
+
+productos_array = productos.to_a
+puts productos_array
+
+productos.delete('galletas')
+puts productos
