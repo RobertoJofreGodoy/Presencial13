100412a123e4eb5c91fcd41f175ef361b985ff6f
RobertoJofreGodoy <roberto.jofre.godoy@gmail.com>
2018-06-12 19:25:49 -0400

    add Ejercicio1

---
+++ Ejercicio1.rb
@@ -0,0 +1,7 @@
+=begin Modificar el hash para utilizar la sintaxis de Ruby 2.0+
+h = { :claveuno => 10, :clavedos => 20, :clavetres => 30 }
+=end
+
+h = { claveuno: 10, clavedos: 20, clavetres: 30 }
+
+puts h
